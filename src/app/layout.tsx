4b764cb8e8ea6dbf7fd9ceede4d379f627ea14d9
samrmdhn <thesyams.rebel8@gmail.com>
2025-11-01 22:54:52 +0700

    fix: done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,45 +1,18 @@
 import React from "react";
-import Script from "next/script";
 import "./globals.css";
-
-async function getTopNavHTML() {
-  const res = await fetch("http://localhost:4000/?username=Alucard", {
-    cache: "no-store",
-  });
-  return res.text();
-}
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const topNavHTML = await getTopNavHTML();
-
-  const props = { username: "Alucard" };
-
   return (
     <html lang="en">
+      <head>
+        {/* Add OC base client so hydration works */}
+        <script src="//localhost:3030/oc-client/client.js" async />
+      </head>
       <body>
-        {/* SSR HTML */}
-        <div
-          id="topnav-root"
-          dangerouslySetInnerHTML={{ __html: topNavHTML }}
-        />
-
-        {/* Props for hydration */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.__TOPNAV_PROPS__ = ${JSON.stringify(props)};`,
-          }}
-        />
-
-        {/* Hydration JS from your micro FE */}
-        <Script
-          src="http://localhost:4000/topnav.client.js"
-          strategy="afterInteractive"
-        />
-
         {children}
       </body>
     </html>
